c2d879c4e43122869a72bdc0e3f480eac294b86a
MinhTuanDev25 <ntmt2502job@gmail.com>
2025-12-27 12:14:37 +0700

    Build a Standard User Database with Migration and create indexes and triggers.

--- internal/db/migrations/000001_users.down.sql
+++ internal/db/migrations/000001_users.down.sql
@@ -1,1 +1,15 @@
-drop table if exists users;
+-- Drop trigger if exist
+DROP TRIGGER IF EXISTS set_user_updated_at on users;
+
+-- Drop tigger function
+DROP FUNCTION IF EXISTS update_user_updated_at_column;
+
+-- Drop index (No need, because deleting the table will also delete the index, but writing it in makes it clearer.)
+DROP INDEX IF EXISTS idx_users_status;
+DROP INDEX IF EXISTS idx_users_level;
+DROP INDEX IF EXISTS idx_users_created_at;
+DROP INDEX IF EXISTS idx_user_deleted_at;
+DROP INDEX IF EXISTS idx_users_email_status;
+
+-- Drop table
+DROP TABLE IF EXISTS users;
